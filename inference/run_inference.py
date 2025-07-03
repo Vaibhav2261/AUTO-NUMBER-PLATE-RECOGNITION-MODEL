e923dec18a2be30d291a02e8acabe3701304d162
Vaibhav Singh <141768389+Vaibhav2261@users.noreply.github.com>
2025-07-04 02:13:10 +0530

    Update run_inference.py

--- inference/run_inference.py
+++ inference/run_inference.py
@@ -4,19 +4,18 @@ import cv2
 from ocr.tesseract_reader import extract_text
 
 model = YOLO("model/best.pt")
-frame = cv2.imread("data/test/sample.jpg")
+img = cv2.imread("data/test/sample.jpg")
 
-results = model(frame)
-
+results = model(img)
 for r in results:
     for box in r.boxes:
         x1, y1, x2, y2 = map(int, box.xyxy[0])
-        plate_crop = frame[y1:y2, x1:x2]
+        plate_crop = img[y1:y2, x1:x2]
         text = extract_text(plate_crop)
         print("Detected Plate:", text)
-        cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
-        cv2.putText(frame, text, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(img, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-cv2.imshow('Detection', frame)
+cv2.imshow("Image ANPR", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
