eaca12815f918a9c31c47fd0ecdea49bfd561a5e
Vaibhav226 <swcric226@gmail.com>
2025-07-04 21:48:33 +0530

    Maikng split of dataset

--- train.py
+++ train.py
@@ -2,7 +2,7 @@
 from ultralytics import YOLO
 
 def train_yolo():
-    model = YOLO('model/yolov8n.pt')  # Load pre-trained model
+    model = YOLO('model/yolov8n.pt')  
     model.train(
         data='anpr.yaml',
         epochs=50,
